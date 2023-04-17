8eb3e3f6aadc7e8d1891201f08bb2f2300df7807
Rafael Gustavo Guillen Mendoza <125158308+RafaelGuillen1999@users.noreply.github.com>
2023-04-17 10:37:29 -0500

    Update script.js

---
+++ Fase_4/script.js
@@ -0,0 +1,22 @@
+function procesarRespuestas () {
+    let total = 3
+    let putos = 0;
+
+    let myFrom = document.forms["formulario"];
+    let respuestasCorrectas = ["c","a","b"];
+
+    for (let i = 1; i <= total; i++) {
+        if (myFrom["r" + i].value == null || 
+            myFrom["r" + i].value == "") {
+            alert("por favor responda todas las preguntas");
+            return false;
+        } else { 
+            if (myFrom["r" + i].value === respuestasCorrectas[i - 1])
+                putos++;
+        }
+          
+    }
+    let resultado = document.getElementById("resultado")
+    resultado.innerHTML=" Obtuviste"+ puntos +"puntos de "+ total +" Posibles ";
+    return false;
+}   
